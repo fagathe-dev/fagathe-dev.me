7a096b273fbd3a2df67d7b2495211f0723587334
fagathe-dev <agathefrederick@gmail.com>
2024-04-21 02:01:29 +0200

    data: ajouter des expériences #43

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Experience;
 use App\Entity\Skill;
 use App\Enum\User\LevelSkillEnum;
+use App\Enum\User\TypeExperienceEnum;
 use App\Enum\User\TypeSkillEnum;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -21,6 +23,18 @@ class AppFixtures extends Fixture
                 ->setLogo($item['logo']);
 
             $manager->persist($skill);
+        }
+        foreach ($this->getExperiences() as $xp) {
+            $experience = new Experience;
+
+            $experience->setName($xp['name'])
+                ->setPlace($xp['place'] ?? null)
+                ->setType($xp['type'])
+                ->setStartYear($xp['start_year'])
+                ->setEndYear($xp['end_year'])
+                ->setTasks($xp['tasks']);
+
+            $manager->persist($experience);
         }
 
         $manager->flush();
@@ -125,6 +139,96 @@ class AppFixtures extends Fixture
                 'name' => 'Terminal',
                 'type' => TypeSkillEnum::TYPE_TOOLS,
                 'logo' => 'icons/terminal.svg',
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getExperiences(): array
+    {
+        return [
+            // Emplois
+            [
+                'name' => 'Analyste développeur junior en alternance',
+                'type' => TypeExperienceEnum::TYPE_EMPLOI,
+                'place' => 'CGI',
+                'start_year' => '2017',
+                'end_year' => '2019',
+                'tasks' => [
+                    'Refonte de pages web avec Wordpress',
+                    'TMA sur une application Web Laravel',
+                    'Rédaction de documentation fonctionnelle',
+                ]
+            ],
+            [
+                'name' => 'Ingénieur informatique',
+                'type' => TypeExperienceEnum::TYPE_EMPLOI,
+                'place' => 'Softia',
+                'start_year' => '2021',
+                'end_year' => '2022',
+                'tasks' => [
+                    'Refonte technique d\'application',
+                    'TMA sur une application Web Laravel et Symfony',
+                    'Rédaction de documentation technico-fonctionnel',
+                    'Gestion de base de données MySQL et PostgreSQL',
+                    '',
+                ]
+            ],
+            [
+                'name' => 'Développeur PHP',
+                'type' => TypeExperienceEnum::TYPE_EMPLOI,
+                'place' => 'Meilleurtaux',
+                'start_year' => '2022',
+                'end_year' => null,
+                'tasks' => [
+                    'Développement d\'application avec Joomla',
+                    'Refonte de formulaire et calculatrice en PHP',
+                    'Développement d\'un back-office en React',
+                    'Développement d\'une interface en ligne de commande avec Nodejs',
+                ]
+            ],
+            // Formations
+            [
+                'name' => 'BTS CGO (Comptabilité Gestion des Organisations)',
+                'type' => TypeExperienceEnum::TYPE_FORMATION,
+                'place' => 'Lycée Roger Verlomme (Paris XV)',
+                'start_year' => '2014',
+                'end_year' => '2016',
+                'tasks' => [
+                    'Bases du SQL',
+                ]
+            ],
+            [
+                'name' => 'BTS TSTR (Technicien Système, Réseaux et Sécurité )',
+                'type' => TypeExperienceEnum::TYPE_FORMATION,
+                'place' => 'IPI (Institut PolyInformatique)',
+                'start_year' => '2017',
+                'end_year' => '2019',
+                'tasks' => [
+                    'Bases de la programmation',
+                    'Java',
+                    'HTML / CSS',
+                    'Javascript',
+                    'PHP',
+                    'SQL',
+                ]
+            ],
+            [
+                'name' => 'Développeur PHP/Symfony (Bac +3/4)',
+                'type' => TypeExperienceEnum::TYPE_FORMATION,
+                'place' => 'OpenClassRooms',
+                'start_year' => '2020',
+                'end_year' => '2021',
+                'tasks' => [
+                    'PHP',
+                    'Javascript niveau intermédiaire',
+                    'Gestion de base de données',
+                    'Rédaction de documentation',
+                    'Wordpress',
+                    'Symfony niveau intermédiaire'
+                ]
             ],
         ];
     }
