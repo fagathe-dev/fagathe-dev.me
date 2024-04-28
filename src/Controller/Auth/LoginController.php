3bd34d78819e36afe24407a75741f24e3da5615b
fagathe-dev <agathefrederick@gmail.com>
2024-04-28 17:35:14 +0200

    front: créer la template auth du BO#5

---
+++ src/Controller/Auth/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Auth;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/login', name: 'auth_login')]
+final class LoginController extends AbstractController
+{
+    
+    
+    #[Route('', name: '')]
+    public function index():Response 
+    {
+        return $this->render('auth/index.html.twig');
+    }
+
+}
