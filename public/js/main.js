85b5d40533eb3df4af4355836c1122e9a8a82826
fagathe-dev <agathefrederick@gmail.com>
2024-06-14 17:22:11 +0200

    front: section masthead #164

--- public/js/main.js
+++ public/js/main.js
@@ -26,3 +26,11 @@ if (dataHref instanceof Element) {
     );
   });
 }
+
+window.onbeforeunload = (e) => {
+  console.log({ type: e.type });
+  confirmMessage = "Vos modifications seront perdues si vous continuer !";
+  e.returnValue = confirmMessage;
+
+  return confirmMessage;
+};
